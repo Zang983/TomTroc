593e739fa083017944ea3e1221d4ac61a54ded4e
KERKAR Kevin <kerkar.kevin@gmail.com>
2024-07-27 23:03:39 +0200

    login page done

--- views/pages/logForm.php
+++ views/pages/logForm.php
@@ -1,22 +1,37 @@
-<form method="POST" action='index.php?action=<?= $_GET["action"] === "inscription" ? "createUser" : "connectUser" ?>'>
-    <?php
-    if ($_GET["action"] === "inscription") { ?>
+<section class="connexion">
+
+    <form method="POST"
+        action='index.php?action=<?= $_GET["action"] === "inscription" ? "createUser" : "connectUser" ?>'>
+        <?php
+        if ($_GET["action"] === "inscription") { ?>
+            <label>
+                Nom d'utilisateur :
+                <input type="text" name="username" autocomplete="off">
+            </label>
+            <?php
+        }
+        ?>
+
         <label>
-            Nom d'utilisateur :
-            <input type="text" name="username" autocomplete="off">
+            Email
+            <input type="text" name="email" autocomplete="username">
         </label>
-    <?php
-    }
-    ?>
+        <label>
+            Mot de passe :
+            <input type="password" name="password">
+        </label>
+        <button type="submit"
+            class="primary_button primary_button--full_width font-semibold"><?= $_GET["action"] === "inscription" ? "S'inscrire" : "Se connecter" ?></button>
+        <?php if ($_GET["action"] === "inscription") { ?>
+            <p>Pas de compte ? <a href="index.php?action=connexion" class="underline">Connectez-vous</a></p>
+            <?php
+        } else { ?>
+            <p>Déjà inscrit ? <a href="index.php?action=inscription" class="underline">Inscrivez-vous</a></p>
 
-    <label>
-        Email
-        <input type="text" name="email" autocomplete="username">
-    </label>
-    <label>
-        Mot de passe :
-        <input type="password" name="password">
-    </label>
-    <button type="submit"><?= $_GET["action"] === "inscription" ? "Inscription" : "Connexion" ?></button>
+            <?php
+        } ?>
+    </form>
 
-</form>
+
+    <img src="assets/connexion.png" alt="">
+</section>
